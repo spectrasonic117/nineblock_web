c78f5b3be2189c8cce90be41b7e4bc1c2b28bdba
Manuel Heredia <Spectrasonic117@gmail.com>
2024-05-14 09:36:50 -0600

    added svelte component

---
+++ src/components/Svelte_Component.svelte
@@ -0,0 +1,10 @@
+<script>
+  let text = "Hello, World!";
+</script>
+
+<section class="bg-peachfuzz-100 p-4 shadow-md">
+  <h1 class="text-peachfuzz-950 font-bold">
+    {text}  
+  </h1>
+  <p class="text-peachfuzz-950">This text is Peachfuzz colored.</p>
+</section>
